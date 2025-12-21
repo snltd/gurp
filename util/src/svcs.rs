2cf08cc8b3352a1f60d34c0affd5e2b4ae7fb211
snltd <github@id264.net>
2025-12-21 17:12:13 +0000

    add wait_for_state(); bugfix set_state()

--- util/src/svcs.rs
+++ util/src/svcs.rs
@@ -1,5 +1,7 @@
 use anyhow::bail;
-use common::constants::{SVCADM_BIN, SVCCFG_BIN, SVCS_BIN};
+use common::constants::{SVC_WAIT_INTERVAL, SVC_WAIT_TIMEOUT, SVCADM_BIN, SVCCFG_BIN, SVCS_BIN};
+use std::thread::sleep;
+use std::time::Duration;
 
 pub fn current_state(svc: &str) -> anyhow::Result<String> {
     cmd_output!(SVCS_BIN, "-Ho", "state", svc)
@@ -34,12 +36,34 @@ pub fn set_state(svc: &str, current_state: &str, desired_state: &str) -> anyhow:
         bail!("unknown or unsupported state: {}", desired_state);
     };
 
-    tracing::info!(
-        "changing svc state: {} {} -> {}",
-        svc,
-        current_state,
-        desired_state
-    );
+    if current_state == desired_state {
+        tracing::debug!("{svc} already in state {desired_state}");
+        Ok(String::new())
+    } else {
+        tracing::info!(
+            "changing svc state: {} {} -> {}",
+            svc,
+            current_state,
+            desired_state
+        );
 
-    cmd_output!(SVCADM_BIN, action, svc)
+        cmd_output!(SVCADM_BIN, action, svc)
+    }
+}
+
+pub fn wait_for_state(svc: &str, state: &str) -> anyhow::Result<bool> {
+    let elapsed = Duration::from_secs(0);
+
+    loop {
+        if current_state(svc)? == state {
+            return Ok(true);
+        }
+
+        sleep(SVC_WAIT_INTERVAL);
+        let elapsed = elapsed + SVC_WAIT_INTERVAL;
+
+        if elapsed >= SVC_WAIT_TIMEOUT {
+            bail!("Timed out waiting for {} be {}", svc, state)
+        }
+    }
 }
