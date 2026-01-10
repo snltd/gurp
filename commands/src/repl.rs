4f1330e1704dd7ec947f543dfd79890072f9838d
snltd <github@id264.net>
2026-01-10 12:02:37 +0000

    add repl command

---
+++ commands/src/repl.rs
@@ -0,0 +1,20 @@
+use common::types::ExitCode;
+use embed::helpers;
+
+pub fn run() -> ExitCode {
+    let client = match helpers::gurp_client() {
+        Ok(client) => client,
+        Err(e) => {
+            tracing::error!("could not create gurp-specific Janet client: {e}");
+            return 1;
+        }
+    };
+
+    match client.run("(repl)") {
+        Ok(_) => 0,
+        Err(e) => {
+            tracing::error!("Janet execution error: {}", e);
+            1
+        }
+    }
+}
