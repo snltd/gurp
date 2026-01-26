43ea57f38bd28a3d65dc9ff53497f2ecde28bedd
snltd <services@id264.net>
2026-01-26 16:39:45 +0000

    fix compilation error

--- commands/src/describe.rs
+++ commands/src/describe.rs
@@ -7,7 +7,7 @@ pub fn run(resource_type: &str) -> ExitCode {
         .map(|(TermWidth(w), _)| w as usize)
         .unwrap_or(80);
 
-    helpers::run_command_and_exit(indoc::formatdod! { r#"
+    helpers::run_command_and_exit(&indoc::formatdoc! { r#"
         (setdyn :term-width {term_width})
         (print (help-for "{resource_type}"))
         "#
