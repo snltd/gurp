e549fb36695c9087584e7b19b419a1effbb3446e
snltd <github@id264.net>
2026-02-01 17:02:52 +0000

    use std::process::ExitCode

--- commands/src/server/init.rs
+++ commands/src/server/init.rs
@@ -1,24 +1,26 @@
 use crate::server;
 use common::types::{ExitCode, ServerOpts};
+use server::http;
+use std::process::ExitCode;
 
 pub fn run(opts: ServerOpts) -> ExitCode {
-    if !opts.config_dir.exists() {
-        tracing::error!("did not find config dir: {}", opts.config_dir);
-        return 1;
-    }
+    if opts.config_dir.exists() {
+        tracing::info!("starting Gurp in server mode");
 
-    tracing::info!("starting Gurp in server mode");
-
-    match server::init::run_server(opts) {
-        Ok(_) => 0,
-        Err(e) => {
-            tracing::error!("server error: {e}");
-            1
+        match run_server(opts) {
+            Ok(_) => ExitCode::SUCCESS,
+            Err(e) => {
+                tracing::error!("server error: {e}");
+                ExitCode::FAILURE
+            }
         }
+    } else {
+        tracing::error!("did not find config dir: {}", opts.config_dir);
+        ExitCode::FAILURE
     }
 }
 
 fn run_server(opts: ServerOpts) -> anyhow::Result<()> {
     let rt = tokio::runtime::Runtime::new()?;
-    rt.block_on(async { server::http::start(opts).await })
+    rt.block_on(async { http::start(opts).await })
 }
