81d6e28619f1bef8a950cca345db1ee92a8aeff2
snltd <github@id264.net>
2026-02-01 17:03:21 +0000

    move macro where it's needed

--- doers/src/types.rs
+++ doers/src/types.rs
@@ -26,8 +26,13 @@ use crate::vlan::{GurpVlanEnsure, GurpVlanRemove};
 use crate::vnic::{GurpVnicEnsure, GurpVnicRemove};
 use crate::zfs::{GurpZfsEnsure, GurpZfsRemove};
 use crate::zone::{GurpZoneEnsure, GurpZoneRemove};
+use anyhow::bail;
+use colored::Colorize;
+use common::types::{ApplyOpts, ApplySummary, ChangedIds};
 use serde::{Deserialize, Serialize};
-use std::collections::BTreeSet; // Because they are hashable
+use serde_json::Error;
+use std::collections::BTreeSet;
+use util::json; // Because they are hashable
 
 #[derive(Deserialize, Debug)]
 pub struct HostConfig {
@@ -164,4 +169,186 @@ pub struct RemoveResources {
     pub zone: Vec<GurpZoneRemove>,
 }
 
-pub type ChangedIds = BTreeSet<String>;
+macro_rules! apply_resources {
+    ($summary_total:ident, $changed_ids:ident, $resources:expr, $opts:expr) => {
+        let total_count = $resources.len();
+        for (i, resource) in $resources.iter().enumerate() {
+            let chunks: Vec<_> = resource.id.split("/").collect();
+            if chunks.len() >= 3 {
+                tracing::debug!(
+                    "applying {} {}/{}: {}",
+                    chunks[1],
+                    i + 1,
+                    total_count,
+                    resource.id
+                );
+            } else {
+                tracing::debug!("applying [{}/{}]: {}", i + 1, total_count, resource.id);
+            }
+            let summary = match resource.apply($opts) {
+                Ok(summary) => summary,
+                Err(e) => {
+                    tracing::error!("from {} doer: {}", chunks[2], e);
+                    let err: anyhow::Error = e.into();
+                    return Err(err.context(format!("failed to apply resource {}", resource.id)));
+                }
+            };
+            $summary_total += summary;
+            if summary.changes > 0 {
+                $changed_ids.insert(resource.id.clone());
+            }
+        }
+    };
+}
+pub struct Applicator {
+    json_config: String,
+}
+
+impl Applicator {
+    pub fn from(json_config: String) -> Self {
+        Self { json_config }
+    }
+
+    pub fn run(&self, opts: &ApplyOpts) -> anyhow::Result<ApplySummary> {
+        let config = self.json_to_hostconfig()?;
+
+        tracing::info!("Configuring host: {}", config.metadata.name);
+        self.apply(&config, opts)
+    }
+
+    fn json_to_hostconfig(&self) -> anyhow::Result<HostConfig> {
+        tracing::debug!(
+            "Unpacking {} bytes of JSON config into HostConfig",
+            self.json_config.len()
+        );
+
+        let host_config: HostConfig = match serde_json::from_str(&self.json_config) {
+            Ok(conf) => conf,
+            Err(e) => {
+                self.display_error(e)?;
+                bail!("end of deserializing output");
+            }
+        };
+
+        Ok(host_config)
+    }
+
+    fn apply(&self, config: &HostConfig, opts: &ApplyOpts) -> anyhow::Result<ApplySummary> {
+        let ensure = &config.resources.ensure;
+        let remove = &config.resources.remove;
+
+        let mut summary_total = ApplySummary::default();
+        let mut changed_ids: ChangedIds = BTreeSet::new();
+
+        apply_resources!(summary_total, changed_ids, &ensure.publisher, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.etherstub, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.vnic, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.vlan, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.ip_interface, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.ip_address, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.bridge, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.route, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.ip_properties, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.network_flow, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.zfs, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.zone, opts);
+
+        if !&ensure.ipnat.is_empty() {
+            summary_total += crate::ipnat::collect_and_ensure(&ensure.ipnat, opts)?;
+        }
+
+        if !&ensure.pkg.is_empty() {
+            summary_total += crate::pkg::collect_and_ensure(&ensure.pkg, opts)?;
+        }
+
+        if !&ensure.pkgin.is_empty() {
+            summary_total += crate::pkgin::collect_and_ensure(&ensure.pkgin, opts)?;
+        }
+
+        if !&ensure.apk.is_empty() {
+            summary_total += crate::apk::collect_and_ensure(&ensure.apk, opts)?;
+        }
+
+        if !&ensure.gem.is_empty() {
+            summary_total += crate::gem::collect_and_ensure(&ensure.gem, opts)?;
+        }
+
+        apply_resources!(summary_total, changed_ids, &ensure.group, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.user, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.cron, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.directory, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.file, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.file_line, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.symlink, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.svcprop, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.smf, opts);
+        apply_resources!(summary_total, changed_ids, &ensure.misc, opts);
+
+        apply_resources!(summary_total, changed_ids, &remove.symlink, opts);
+        apply_resources!(summary_total, changed_ids, &remove.file_line, opts);
+        apply_resources!(summary_total, changed_ids, &remove.file, opts);
+        apply_resources!(summary_total, changed_ids, &remove.directory, opts);
+        apply_resources!(summary_total, changed_ids, &remove.svcprop, opts);
+        apply_resources!(summary_total, changed_ids, &remove.smf, opts);
+        apply_resources!(summary_total, changed_ids, &remove.cron, opts);
+        apply_resources!(summary_total, changed_ids, &remove.user, opts);
+        apply_resources!(summary_total, changed_ids, &remove.group, opts);
+        apply_resources!(summary_total, changed_ids, &remove.publisher, opts);
+
+        if !&remove.gem.is_empty() {
+            summary_total += crate::gem::collect_and_remove(&remove.gem, opts)?;
+        }
+
+        if !&remove.pkg.is_empty() {
+            summary_total += crate::pkg::collect_and_remove(&remove.pkg, opts)?;
+        }
+
+        if !&remove.pkgin.is_empty() {
+            summary_total += crate::pkgin::collect_and_remove(&remove.pkgin, opts)?;
+        }
+
+        if !&remove.apk.is_empty() {
+            summary_total += crate::apk::collect_and_remove(&remove.apk, opts)?;
+        }
+
+        apply_resources!(summary_total, changed_ids, &remove.ipnat, opts);
+        apply_resources!(summary_total, changed_ids, &remove.zone, opts);
+        apply_resources!(summary_total, changed_ids, &remove.zfs, opts);
+        apply_resources!(summary_total, changed_ids, &remove.bridge, opts);
+        apply_resources!(summary_total, changed_ids, &remove.network_flow, opts);
+        apply_resources!(summary_total, changed_ids, &remove.route, opts);
+        apply_resources!(summary_total, changed_ids, &remove.ip_address, opts);
+        apply_resources!(summary_total, changed_ids, &remove.ip_interface, opts);
+        apply_resources!(summary_total, changed_ids, &remove.vlan, opts);
+        apply_resources!(summary_total, changed_ids, &remove.vnic, opts);
+        apply_resources!(summary_total, changed_ids, &remove.etherstub, opts);
+
+        for resource in &ensure.svc {
+            summary_total += resource.apply(&changed_ids, opts)?;
+        }
+
+        Ok(summary_total)
+    }
+
+    fn display_error(&self, e: Error) -> anyhow::Result<()> {
+        tracing::error!("deserializing error: {}", e);
+
+        let formatted_json = json::formatted(&self.json_config)?;
+        let error_line = e.line();
+        let json_lines: Vec<_> = formatted_json.lines().collect();
+        let first_line = error_line.saturating_sub(30);
+        let last_line = (error_line + 15).clamp(0, json_lines.len());
+
+        for l in first_line..=last_line {
+            let output_line = format!(" {:4} | {}", l + 1, json_lines.get(l).unwrap_or(&""));
+
+            if l == error_line {
+                println!("{}", output_line.bold());
+            } else {
+                println!("{output_line}");
+            }
+        }
+
+        Ok(())
+    }
+}
